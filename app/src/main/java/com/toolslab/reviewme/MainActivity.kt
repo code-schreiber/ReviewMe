2723fcdb809dfc866ca013d4ae1d0ee672b2f129
code-schreiber <code-schreiber@users.noreply.github.com>
2019-06-07 11:51:29 +0200

    👓✔️ Add basic files

---
+++ app/src/main/java/com/toolslab/reviewme/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.toolslab.reviewme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
